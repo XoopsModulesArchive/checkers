86e833a775dad8341fd6c08d6bb08eb4e00df65a
mambax7 <mambax7@gmail.com>
2020-07-11 22:44:28 -0400

    initial
    Signed-off-by: mambax7 <mambax7@gmail.com>

---
+++ xoops_version.php
@@ -0,0 +1,17 @@
+<?php
+$modversion['name'] = "3-in-1_Checkers";
+$modversion['version'] = 1.00;
+$modversion['description'] = "Flash games hosted on your site.";
+$modversion['author'] = "Ported To Xoops By Shadow (<a href='http://shadowsbastion.com/'>Shadow's Bastion</a>)";
+$modversion['credits'] = "3 in 1 checkers Created by Ddot Design(<a href='http://www.webgamedesign.com/'>Ddot Design</a>)";
+$modversion['help'] = "";
+$modversion['license'] = "GPL see LICENSE";
+$modversion['official'] = 0;
+$modversion['image'] = "images/checkers.jpg";
+$modversion['dirname'] = "checkers";
+$modversion['hasAdmin'] = 0;
+$modversion['adminpath'] = "";
+$modversion['hasMain'] = 1;
+$modversion['templates'][1]['file'] = "checker.html";
+$modversion['templates'][1]['description'] = "The template file of index";
+?>
